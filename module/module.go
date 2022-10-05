23a9259777f352ea2ab7a8c624726a16f2319227
Xinan Xu <39632914+xinaxu@users.noreply.github.com>
2022-10-05 16:56:54 -0700

    Add comments

--- module/module.go
+++ module/module.go
@@ -11,9 +11,19 @@ type Marshallable interface {
 }
 
 type Module interface {
+	// TaskType is a field that can be used to identify the test type
 	TaskType() task.Type
+
+	// ResultType is the type of the result that can be used to create database tables
 	ResultType() interface{}
+
+	// GetTasks returns task inputs that should be executed according to certain restriction
+	// such as priority or number of concurrent task for each target
 	GetTasks([]task.Definition) (map[task.Definition]Marshallable, error)
+
+	// GetTask generates the task input from task definition
 	GetTask(task.Definition) (Marshallable, error)
+
+	// Validate accepts the task input and returns the validation result
 	Validate(ctx context.Context, input []byte) ([]byte, error)
 }
