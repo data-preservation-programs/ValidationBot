8c17352ca0069faebbbbdbdf6136ce847fb786a4
Xinan Xu <39632914+xinaxu@users.noreply.github.com>
2022-12-13 22:15:23 -0800

    Example rpc server that selects and output a random port

---
+++ cmd/rpc.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"net"
+	"net/http"
+	"net/rpc"
+)
+
+type Args struct {
+	A, B int
+}
+
+type Quotient struct {
+	Quo, Rem int
+}
+
+type Arith int
+
+func (t *Arith) Multiply(args *Args, reply *int) error {
+	*reply = args.A * args.B
+	return nil
+}
+
+func (t *Arith) Divide(args *Args, quo *Quotient) error {
+	if args.B == 0 {
+		return errors.New("divide by zero")
+	}
+	quo.Quo = args.A / args.B
+	quo.Rem = args.A % args.B
+	return nil
+}
+
+func main() {
+
+	arith := new(Arith)
+	rpc.Register(arith)
+	rpc.HandleHTTP()
+
+	listener, _ := net.Listen("tcp", ":0")
+	fmt.Println("Using port:", listener.Addr().(*net.TCPAddr).Port)
+	http.Serve(listener, nil)
+}
