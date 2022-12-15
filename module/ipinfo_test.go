b7d3055fa993f2f29b8cfd56df34c0755b8eaebd
brianrossetti <brossetti1@gmail.com>
2022-12-14 20:07:05 -0800

    build ipinfo service

--- module/ipinfo_test.go
+++ module/ipinfo_test.go
@@ -9,48 +9,36 @@ import (
 
 func TestIpInfoResolver_ResolveIP(t *testing.T) {
 	assert := assert.New(t)
-	resolver, err := NewIpInfoResolver()
+	resolver := IpInfoResolver{}
+	country, err := resolver.ResolveIPStr("66.66.66.66")
 	assert.Nil(err)
-	city, err := resolver.ResolveIPStr("66.66.66.66")
-	assert.Nil(err)
-	assert.Equal("US", city.Country.IsoCode)
-	assert.Equal("NA", city.Continent.Code)
-	assert.Equal("NY", city.Subdivisions[0].IsoCode)
-	assert.Equal("Endicott", city.City.Names["en"])
+	assert.Equal("US", country)
 }
 
 func TestIpInfoResolver_ResolveIP_Local(t *testing.T) {
 	assert := assert.New(t)
-	resolver, err := NewIpInfoResolver()
+	resolver := IpInfoResolver{}
+	country, err := resolver.ResolveIPStr("192.168.1.1")
 	assert.Nil(err)
-	city, err := resolver.ResolveIPStr("192.168.1.1")
-	assert.Nil(err)
-	assert.Equal("", city.Country.IsoCode)
-	assert.Equal("", city.Continent.Code)
+	assert.Equal("", country)
 }
 
 func TestIpInfoResolver_ResolveMultiAddr(t *testing.T) {
 	assert := assert.New(t)
-	resolver, err := NewIpInfoResolver()
-	assert.Nil(err)
+	resolver := IpInfoResolver{}
 	addr, err := multiaddr.NewMultiaddr("/ip4/66.66.66.66/tcp/80")
 	assert.Nil(err)
-	city, err := resolver.ResolveMultiAddr(addr)
+	country, err := resolver.ResolveMultiAddr(addr)
 	assert.Nil(err)
-	assert.Equal("US", city.Country.IsoCode)
-	assert.Equal("NA", city.Continent.Code)
-	assert.Equal("NY", city.Subdivisions[0].IsoCode)
-	assert.Equal("Endicott", city.City.Names["en"])
+	assert.Equal("US", country)
 }
 
 func TestIpInfoResolver_ResolveMultiAddrDNS4(t *testing.T) {
 	assert := assert.New(t)
-	resolver, err := NewIpInfoResolver()
-	assert.Nil(err)
+	resolver := IpInfoResolver{}
 	addr, err := multiaddr.NewMultiaddr("/dns4/www.google.com/tcp/80")
 	assert.Nil(err)
-	city, err := resolver.ResolveMultiAddr(addr)
+	country, err := resolver.ResolveMultiAddr(addr)
 	assert.Nil(err)
-	assert.Equal("US", city.Country.IsoCode)
-	assert.Equal("NA", city.Continent.Code)
+	assert.Equal("US", country)
 }
