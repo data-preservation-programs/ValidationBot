5e6dd82e39df9682cc704dc5e4fc0b3c898ae796
brianrossetti <brossetti1@gmail.com>
2022-12-15 22:01:24 -0800

    cleaning up draft

--- role/auditor/rpc.go
+++ role/auditor/rpc.go
@@ -26,31 +26,6 @@ type RpcClient struct {
 	baseDir string
 }
 
-// for testing
-type Args struct {
-	A, B int
-}
-
-type Quotient struct {
-	Quo, Rem int
-}
-
-type Arith int
-
-func (t *Arith) Multiply(args *Args, reply *int) error {
-	*reply = args.A * args.B
-	return nil
-}
-
-func (t *Arith) Divide(args *Args, quo *Quotient) error {
-	if args.B == 0 {
-		return errors.New("divide by zero")
-	}
-	quo.Quo = args.A / args.B
-	quo.Rem = args.A % args.B
-	return nil
-}
-
 func (r *RpcClient) Call(ctx context.Context, input module.ValidationInput) (*module.ValidationResult, error) {
 	uuidStr, err := uuid.NewUUID()
 	if err != nil {
--- rpc/validation_server.go
+++ rpc/validation_server.go
@@ -22,16 +22,14 @@ type RpcAuditor struct {
 func (ra RpcAuditor) Validate(ctx context.Context, input module.ValidationInput) (*module.ValidationResult, error) {
 	ra.log.Info().Msgf("Received validation request for task %s", input.TaskID)
 
-	modType := input.Type
-
-	mod, ok := ra.modules[modType]
+	mod, ok := ra.modules[input.Type]
 	if !ok {
 		return nil, errors.New(fmt.Sprintf("no module found for task type %s", input.Type))
 	}
 
 	result, err := mod.Validate(ctx, input)
 	if err != nil {
-		fmt.Printf("Error validating task %s: %s", input.TaskID, err)
+		fmt.Printf("Error validating task %s: %s", input.Type, err)
 		return nil, errors.Wrap(err, "failed validating task")
 	}
 
