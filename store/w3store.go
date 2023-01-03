50430f711b4f66047db10bccda13eceb3d321fde
brianrossetti <brossetti1@gmail.com>
2023-01-03 11:20:05 -0700

    adding detailed error messages

--- store/w3store.go
+++ store/w3store.go
@@ -74,7 +74,7 @@ func (s W3StoreSubscriber) Head(ctx context.Context, peerID peer.ID) (*Entry, er
 
 	latest, err := getLastRecord(ctx, log, s.client, peerStr)
 	if err != nil {
-		return nil, errors.Wrap(err, "failed to get last record")
+		return nil, errors.Wrap(err, "failed to get last record (Head)")
 	}
 
 	if latest == nil {
@@ -122,7 +122,7 @@ func (s W3StoreSubscriber) Subscribe(ctx context.Context, peerID peer.ID, last *
 		for {
 			latest, err := getLastRecord(ctx, log, s.client, peerStr)
 			if err != nil {
-				log.Error().Err(err).Msg("failed to get last record")
+				log.Error().Err(err).Msg("failed to get last record (Subscribe)")
 				time.Sleep(s.retryInterval)
 				continue
 			}
@@ -339,7 +339,7 @@ func (s *W3StorePublisher) initialize(ctx context.Context) error {
 
 	entry, err := getLastRecord(ctx, s.log, s.client, peerStr)
 	if err != nil {
-		return errors.Wrap(err, "failed to get last record")
+		return errors.Wrap(err, "failed to get last record (init)")
 	}
 
 	if entry != nil {
@@ -347,7 +347,7 @@ func (s *W3StorePublisher) initialize(ctx context.Context) error {
 
 		lastCid, err := cid.Decode(string(entry.Value))
 		if err != nil {
-			return errors.Wrap(err, "failed to get last cid")
+			return errors.Wrap(err, "failed to get last cid (init)")
 		}
 
 		s.lastCid = &lastCid
