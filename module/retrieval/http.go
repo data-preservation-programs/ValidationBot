d02963b013944ff916bd16ffb6a1f03f5ab45357
brianrossetti <brossetti1@gmail.com>
2023-01-19 17:06:51 -0700

    wip

---
+++ module/retrieval/http.go
@@ -0,0 +1,70 @@
+package retrieval
+
+import (
+	"context"
+	"fmt"
+	"sort"
+	"strings"
+
+	"github.com/filecoin-project/boost/cmd"
+	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/lotus/api"
+)
+
+func getMinerProtocols(ctx context.Context, api api.FullNode, miner address.Address) ([]string, error) {
+	api, closer, err := lcli.GetGatewayAPI(cctx)
+	if err != nil {
+		return fmt.Errorf("setting up gateway connection: %w", err)
+	}
+	defer closer()
+
+	addrStr := cctx.Args().Get(0)
+	maddr, err := address.NewFromString(addrStr)
+	if err != nil {
+		return fmt.Errorf("parsing provider on-chain address %s: %w", addrStr, err)
+	}
+
+	addrInfo, err := cmd.GetAddrInfo(ctx, api, maddr)
+	if err != nil {
+		return fmt.Errorf("getting provider multi-address: %w", err)
+	}
+
+	log.Debugw("connecting to storage provider",
+		"id", addrInfo.ID, "multiaddrs", addrInfo.Addrs, "addr", maddr)
+
+	if err := n.Host.Connect(ctx, *addrInfo); err != nil {
+		return fmt.Errorf("connecting to peer %s: %w", addrInfo.ID, err)
+	}
+
+	protos, err := n.Host.Peerstore().GetProtocols(addrInfo.ID)
+	if err != nil {
+		return fmt.Errorf("getting protocols for peer %s: %w", addrInfo.ID, err)
+	}
+	sort.Strings(protos)
+
+	agentVersionI, err := n.Host.Peerstore().Get(addrInfo.ID, "AgentVersion")
+	if err != nil {
+		return fmt.Errorf("getting agent version for peer %s: %w", addrInfo.ID, err)
+	}
+	agentVersion, _ := agentVersionI.(string)
+
+	if cctx.Bool("json") {
+		return cmd.PrintJson(map[string]interface{}{
+			"provider":   addrStr,
+			"agent":      agentVersion,
+			"id":         addrInfo.ID.String(),
+			"multiaddrs": addrInfo.Addrs,
+			"protocols":  protos,
+		})
+	}
+
+	fmt.Println("Provider: " + addrStr)
+	fmt.Println("Agent: " + agentVersion)
+	fmt.Println("Peer ID: " + addrInfo.ID.String())
+	fmt.Println("Peer Addresses:")
+	for _, addr := range addrInfo.Addrs {
+		fmt.Println("  " + addr.String())
+	}
+	fmt.Println("Protocols:\n" + "  " + strings.Join(protos, "\n  "))
+	return nil
+}
